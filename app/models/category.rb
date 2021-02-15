8287af2b1eb662b1301d3284b1ec9bdd93da4258
shushikata <shu.42111227@gmail.com>
2021-02-16 00:12:28 +0900

    実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :ideas, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 end
--- db/migrate/20210214190922_create_ideas.rb
+++ db/migrate/20210214190922_create_ideas.rb
@@ -1,7 +1,7 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
-      t.bigint :category_id, null: false
+      t.references :category, null: false, foreign_key: true
       t.text :body, null: false, comment: "アイデア本文"
 
       t.timestamps
